9d784072b3f188eda4786d0a269dc7faa32d4f6e
Jason Raimondi <jason@raimondi.us>
2016-09-21 17:11:41 -0700

    Add mobile homescreen meta tag.

--- resources/views/index.php
+++ resources/views/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="mobile-web-app-capable" content="yes">
     <title>Flipp</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.3/foundation.min.css" />
     <script src="./assets/package.js"></script>
